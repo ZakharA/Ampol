ff1890e8fff3900ac8fdb9c03df3eb74784e2555
Arsentii Zakharchenko <ZakharA@users.noreply.github.com>
2025-11-08 13:36:20 +1100

    change name for discount to discount promotion to match with requirements

--- src/DiscountAPI.Core/Models/DiscountPromotion.cs
+++ src/DiscountAPI.Core/Models/DiscountPromotion.cs
@@ -1,10 +1,10 @@
 namespace DiscountAPI.Core.Models;
 
-public class Discount
+public class DiscountPromotion
 {
     public string Id { get; set; } = string.Empty;
-    public string Name { get; set; } = string.Empty;
+    public string PromotionName { get; set; } = string.Empty;
     public DateTimeOffset StartDate { get; set; }
     public DateTimeOffset EndDate { get; set; }
-    public decimal DiscountPercentage { get; set; }
+    public decimal DiscountPercent { get; set; }
 }
---
+++ src/DiscountAPI.Core/Models/DiscountPromotionProduct.cs
@@ -0,0 +1,7 @@
+namespace DiscountAPI.Core.Models;
+
+public class DiscountPromotionProduct
+{
+    public string DiscountPromotionId { get; set; } = string.Empty;
+    public string ProductId { get; set; } = string.Empty;
+}
